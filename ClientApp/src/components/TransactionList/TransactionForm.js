90c8b2511dfac19410273e3cfa899c7a1497a929
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-01-06 17:15:55 +1030

    Use Formik for forms

--- ClientApp/src/components/TransactionList/TransactionForm.js
+++ ClientApp/src/components/TransactionList/TransactionForm.js
@@ -11,14 +11,9 @@ import {
 } from "reactstrap";
 import { toDatetimeLocalInputDate } from "../../utils/DateExtensions";
 import { validatedTransaction } from "../../utils/TransactionValidation";
+import { Formik, useField } from "formik";
 
-const TransactionForm = ({
-  formId,
-  transaction,
-  categories,
-  submitCallback,
-  isSelectable,
-}) => {
+const TransactionForm = ({ transaction, categories, submitCallback }) => {
   const [formData, setFormData] = useState({
     description: "",
     amount: "",
@@ -41,91 +36,98 @@ const TransactionForm = ({
     }
   }, [transaction, categories]);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((data) => ({ ...data, [name]: value }));
+  const formik = ({
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    isSubmitting,
+  }) => {
+    const description = (
+      <FormInput type="text" name="description" placeholder="Description" />
+    );
+
+    const timestamp = <FormInput type="datetime-local" name="timestamp" />;
+
+    const category = (
+      <FormInput name="category" as="select" type="select">
+        <option value={null}> Uncategorised </option>
+        {categories.map((c) => (
+          <option key={c.id} value={c.id}>
+            {c.name}
+          </option>
+        ))}
+      </FormInput>
+    );
+
+    const amount = (
+      <Row>
+        <Col xs={5} style={{ paddingRight: 5 }}>
+          <FormInput name="amountSign" as="select" type="select">
+            <option value={"-"}>-</option>
+            <option value={"+"}>+</option>
+          </FormInput>
+        </Col>
+        <Col xs={7} style={{ paddingLeft: 5 }}>
+          <InputGroup size="sm">
+            <InputGroupText>$</InputGroupText>
+            <FormInput
+              name="amount"
+              placeholder="Amount"
+              type="number"
+              min="0.01"
+              step="0.01"
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+    );
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <TransactionTable
+          transaction={{
+            description: description,
+            timestamp: timestamp,
+            amount: amount,
+            category: category,
+            submit: (
+              <Button type="submit" size="sm" outline disabled={isSubmitting}>
+                <i className="fa-solid fa-floppy-disk"></i>
+              </Button>
+            ),
+          }}
+        />
+      </Form>
+    );
   };
-
-  const submit = (event) => {
-    event.preventDefault();
-    let transaction = validatedTransaction(formData);
-    submitCallback(transaction);
-  };
-
-  const FormInput = (props) => (
-    <Input
-      {...props}
-      defaultValue={formData[props.name]}
-      onChange={handleChange}
-      bsSize="sm"
-    />
-  );
-
-  const SelectFormInput = (props) => (
-    <Input
-      {...props}
-      type="select"
-      value={formData[props.name]}
-      onChange={handleChange}
-      bsSize="sm"
-    >
-      {props.children}
-    </Input>
-  );
-
-  const description = (
-    <FormInput name="description" placeholder="Description" type="text" />
-  );
-
-  const timestamp = <FormInput name="timestamp" type="datetime-local" />;
-
-  const amount = (
-    <Row>
-      <Col xs={5} style={{ paddingRight: 5 }}>
-        <SelectFormInput name="amountSign">
-          <option value={"-"}>-</option>
-          <option value={"+"}>+</option>
-        </SelectFormInput>
-      </Col>
-      <Col xs={7} style={{ paddingLeft: 5 }}>
-        <InputGroup size="sm">
-          <InputGroupText>$</InputGroupText>
-          <FormInput
-            name="amount"
-            placeholder="Amount"
-            type="number"
-            min="0.01"
-            step="0.01"
-          />
-        </InputGroup>
-      </Col>
-    </Row>
-  );
-
-  const category = (
-    <SelectFormInput name="category">
-      <option value={null}> Uncategorised </option>
-      {categories.map((c) => (
-        <option key={c.id} value={c.id}>
-          {c.name}
-        </option>
-      ))}
-    </SelectFormInput>
-  );
 
   return (
-    <Form onSubmit={submit} id={formId} className="transaction-form">
-      <TransactionTable
-        isSelectable={isSelectable}
-        transaction={{
-          description: description,
-          timestamp: timestamp,
-          amount: amount,
-          category: category,
-          submit: <Button type="submit" size="sm" outline><i className="fa-solid fa-floppy-disk"></i></Button>,
-        }}
-      />
-    </Form>
+    <Formik
+      enableReinitialize
+      initialValues={formData}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setSubmitting(false);
+        let transaction = validatedTransaction(values);
+        submitCallback(transaction);
+      }}
+    >
+      {formik}
+    </Formik>
+  );
+};
+
+const FormInput = ({ label, ...props }) => {
+  const [field, meta, helpers] = useField(props);
+  return (
+    <>
+      <Input bsSize="sm" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
   );
 };
 
