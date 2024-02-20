052a51d452879608705043db71d599c33004c19a
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-02-21 10:21:19 +1030

    Add Edit/Save buttons for monthly targets

--- ClientApp/src/components/MonthlyTarget/MonthlyTargetIndex.tsx
+++ ClientApp/src/components/MonthlyTarget/MonthlyTargetIndex.tsx
@@ -8,6 +8,7 @@ const MonthlyTargetIndex = () => {
 
   const [token, setToken] = useState("");
   const [targets, setTargets] = useState<Array<MonthlyTarget>>([]);
+  const [editId, setEditId] = useState("");
 
   useEffect(() => {
     getAccessTokenSilently().then(data => setToken(data));
@@ -21,27 +22,38 @@ const MonthlyTargetIndex = () => {
       setTargets(data);
     });
   }, [token]);
-  
+
   return (
     <>
       <div className="p-3">
         <h3>Monthly Targets</h3>
         <table className="table-auto border-collapse">
           <thead>
-            <tr>
-              <th className="border-b text-left p-3">Until</th>
-              <th className="border-b text-left p-3">Amount</th>
-            </tr>
+          <tr>
+            <th className="border-b text-left p-3">Until</th>
+            <th className="border-b text-left p-3">Amount</th>
+            <th className="border-b text-left p-3"></th>
+          </tr>
           </thead>
           <tbody>
-            {targets.map(target =>
-              <tr key={target.id}>
-                <td className="border-b p-3">
-                  <input type="number" value={target.month} className="w-10" disabled/>/
-                  <input type="number" value={target.year} className="w-20" disabled/>
-                </td>
-                <td className="border-b p-3">$<input type="number" value={target.amount} disabled/></td>
-              </tr>)}
+          {targets.map(target => {
+            const isEdited = editId != target.id;
+            return <tr key={target.id}>
+              <td className="border-b p-3">
+                <input type="number" value={target.month} className="w-10" disabled={isEdited}/>/
+                <input type="number" value={target.year} className="w-20" disabled={isEdited}/>
+              </td>
+              <td className="border-b p-3">$<input type="number" value={target.amount} disabled={isEdited}/></td>
+              <td className="border-b p-3">
+                {
+                  isEdited
+                    ? <button onClick={() => setEditId(target.id ?? "")}>Edit</button>
+                    : <button>Save</button>
+                }
+              </td>
+            </tr>
+              ;
+          })}
           </tbody>
         </table>
       </div>
