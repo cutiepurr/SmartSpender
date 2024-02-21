09a465b4daf62d2d036e29c1330cb6eeb45dd5de
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-02-21 15:15:58 +1030

    Use useFormik for transaction form

--- ClientApp/src/components/Transaction/Forms/TransactionForm.tsx
+++ ClientApp/src/components/Transaction/Forms/TransactionForm.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
-import TransactionTable from "../TransactionTable";
 import {toDatetimeLocalInputDate} from "../../../utils/DateExtensions";
 import {validatedTransaction,} from "../../../utils/TransactionValidation";
-import {Field, Formik, useField} from "formik";
+import {useFormik} from "formik";
 import {apiToFormTransaction, ApiTransaction, formToApiTransaction, FormTransaction} from "../../../utils/Transaction";
 import {useAuth0} from "@auth0/auth0-react";
 import {CategoryItem} from "@/utils/Category";
+import TransactionTable from "../TransactionTable";
 
 interface props {
   transaction: ApiTransaction,
@@ -26,6 +26,18 @@ const TransactionForm: React.FC<props> = ({transaction, categories, submitCallba
   };
 
   const [formData, setFormData] = useState(blankFormTransaction);
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: formData,
+    onSubmit: (values, {setSubmitting}) => {
+      setSubmitting(false);
+      let transaction = validatedTransaction(values);
+      if (transaction == null) return;
+
+      let apiTransaction = formToApiTransaction(transaction);
+      submitCallback(apiTransaction);
+    }
+  });
 
   useEffect(() => {
     if (transaction === null) return;
@@ -34,130 +46,59 @@ const TransactionForm: React.FC<props> = ({transaction, categories, submitCallba
     setFormData(formTransaction);
   }, [transaction, categories]);
 
-  const formik = ({handleSubmit, isSubmitting}) => {
-    const description = (
-      <FormInput
-        label="description"
-        type="text"
-        name="description"
-        placeholder="Description"
-        // validate={Validation.description}
-      />
-    );
-
-    const timestamp = (
-      <FormInput
-        label="timestamp"
-        type="datetime-local"
-        name="timestamp"
-        // validate={Validation.date}
-      />
-    );
-
-    const category = (
-      <FormSelect label="categoryId" name="categoryId">
-        {categories.map((c) => (
-          <option key={c.categoryId} value={c.categoryId}>
-            {c.name}
-          </option>
-        ))}
-      </FormSelect>
-    );
-
-    const amount = (
-      <div className="w-full flex flex-row">
-        <FormSelect label="amountSign" name="amountSign">
-          <option value={"-"}>-</option>
-          <option value={"+"}>+</option>
-        </FormSelect>
-        <div>$</div>
-        <FormInput
-          label="amount"
-          name="amount"
-          placeholder="Amount"
-          type="number"
-          min="0.01"
-          step="0.01"
-          // validate={Validation.amount}
-        />
-      </div>
-    );
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <TransactionTable
-          transaction={{
-            description: description,
-            timestamp: timestamp,
-            amount: amount,
-            category: category,
-            submit: (
-              <button type="submit" disabled={isSubmitting}>
-                <i className="fa-solid fa-floppy-disk"></i>
-              </button>
-            ),
-          }}
-        />
-      </form>
-    );
-  };
-
-  return (
-    <Formik
-      enableReinitialize
-      initialValues={formData}
-      onSubmit={(values, {setSubmitting}) => {
-        setSubmitting(false);
-        let transaction = validatedTransaction(values);
-        if (transaction == null) return;
-
-        let apiTransaction = formToApiTransaction(transaction);
-        submitCallback(apiTransaction);
-      }}
-    >
-      {formik}
-    </Formik>
+  const description = (
+    <input type="text" name="description" placeholder="Description"
+           onChange={formik.handleChange} value={formik.values.description}
+      // validate={Validation.description}
+    />
   );
-};
 
-const FormSelect = ({label, ...props}) => {
-  // @ts-ignore
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <select
-        {...field}
-        {...props}
-        // @ts-ignore
-        // invalid={meta.touched && meta.error}
-      ></select>
-      {meta.touched && meta.error ? (
-        <div style={{zIndex: 1}} className="error">
-          {meta.error}
-        </div>
-      ) : null}
-    </>
+  const timestamp = (
+    <input type="datetime-local" name="timestamp" onChange={formik.handleChange} value={formik.values.timestamp}
+      // validate={Validation.date}
+    />
   );
-};
 
-const FormInput = ({label, ...props}) => {
-  // @ts-ignore
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <input
-        className="w-full"
-        {...field}
-        {...props}
-        // @ts-ignore
-        // invalid={meta.touched && meta.error}
+  const category = (
+    <select name="categoryId" onChange={formik.handleChange} value={formik.values.categoryId}>
+      {categories.map((c) => (
+        <option key={c.categoryId} value={c.categoryId}>
+          {c.name}
+        </option>
+      ))}
+    </select>
+  );
+
+  const amount = (
+    <div className="w-full flex flex-row">
+      <select name="amountSign" onChange={formik.handleChange} value={formik.values.amountSign}>
+        <option value={"-"}>-</option>
+        <option value={"+"}>+</option>
+      </select>
+      <div>$</div>
+      <input name="amount" placeholder="Amount" type="number" min="0.01" step="0.01"
+             onChange={formik.handleChange} value={formik.values.amount}
+        // validate={Validation.amount}
       />
-      {meta.touched && meta.error ? (
-        <div style={{zIndex: 1}} className="error">
-          {meta.error}
-        </div>
-      ) : null}
-    </>
+    </div>
+  );
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <TransactionTable
+        transaction={{
+          description: description,
+          timestamp: timestamp,
+          amount: amount,
+          category: category,
+          submit: (
+            <button type="submit" disabled={formik.isSubmitting}>
+              <i className="fa-solid fa-floppy-disk"></i>
+            </button>
+          ),
+        }}
+      />
+    </form>
   );
 };
 
