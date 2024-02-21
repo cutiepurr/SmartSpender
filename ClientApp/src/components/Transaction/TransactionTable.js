58dd724c9c8203faf9fef5062de7b39136afc3aa
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-02-21 15:32:20 +1030

    Reduce nesting in the transaction table

--- ClientApp/src/components/Transaction/TransactionTable.js
+++ ClientApp/src/components/Transaction/TransactionTable.js
@@ -1,33 +1,24 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 
-const TransactionTable = (prop) => {
-  const {
-    transaction,
-    className = "",
-    onClick,
-  } = prop;
+const TransactionTable = ({transaction, ...props}) => {
   return (
-    <div className={`${className}`} style={{ minHeight: 50 }}>
-      <div className="py-2">
-        <div onClick={onClick}>
-          <div className="grid grid-cols-1 md:grid-cols-12 gap-2">
-            <div className="col-span-3">
-              <div>{transaction.timestamp}</div>
-            </div>
-            <div className="col-span-4">
-              <div>{transaction.description}</div>
-            </div>
-            <div className="col-span-2">
-              <div>{transaction.amount}</div>
-            </div>
-            <div className="col-span-2">
-              <div>{transaction.category}</div>
-            </div>
-            <div className="col-span-1">
-              <div>{transaction.submit}</div>
-            </div>
-          </div>
+    <div style={{ minHeight: 50 }} {...props}>
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-2 py-2">
+        <div className="col-span-3">
+          {transaction.timestamp}
+        </div>
+        <div className="col-span-4">
+          {transaction.description}
+        </div>
+        <div className="col-span-2">
+          {transaction.amount}
+        </div>
+        <div className="col-span-2">
+          {transaction.category}
+        </div>
+        <div className="col-span-1">
+          {transaction.submit}
         </div>
       </div>
     </div>
